a1029dbee0fd8426c67f3246865bb98e963967db
Samuel Nelson <snelson54@cnm.edu>
2019-04-10 10:46:37 -0600

    Filled in details.

--- epic/persona.php
+++ epic/persona.php
@@ -6,16 +6,23 @@
 	</head>
 	<body>
 		<h1>Persona</h1>
-			<img src="dentist.jpg" alt="Alfred and his dog." />
-		<ul>
-			<li><b>Name:</b> Alfred Derfla</li>
-			<li><b>Occupation:</b> Dentist</li>
-			<li><b>Gender:</b> Male</li>
-			<li><b>Age:</b> 32</li>
-			<li><b>Technology:</b> Newest Apple everything. He's got it all. iMac, Macbook, iPhone.</li>
-			<li><b>Technical proficiency:</b> 9</li>
-		</ul>
-		<a href="index.php">Home</a>
+				<img src="dentist.jpg" alt="Alfred and his dog." />
+			<ul>
+				<li><b>Name:</b> Alfred Derfla</li>
+				<li><b>Occupation:</b> Dentist</li>
+				<li><b>Gender:</b> Male</li>
+				<li><b>Age:</b> 32</li>
+				<li><b>Technology:</b> Newest Apple everything. He's got it all. iMac, MacBook, iPhone.</li>
+				<li><b>Technical proficiency:</b> 9</li>
+			</ul>
+		<h2>Things Alfred likes</h2>
+			<ul>
+				<li>Doggos</li>
+				<li>Light-hearted fun</li>
+				<li>Conntecting with like minded people</li>
+				<li>Expressing his opinion</li>
+			</ul>
+			<a href="index.php">Home</a>
 
 
 	</body>
--- epic/user-story.php
+++ epic/user-story.php
@@ -6,20 +6,8 @@
 	</head>
 	<body>
 		<h1>User Story</h1>
-		<p>Alfred is a Dentist. He regularly works long shifts and</p>
-		<p>sometimes just needs a break when he's done for the day.</p>
-		<p>He finds it relaxing to browse r/tippytaps to see some </p>
-		<p>h*ckin good doggos. But he wants to not only see the posts,</p>
-		<p>but to interact with them and leave comments to express</p>
-		<p>the overwhelming joy he feels.</p>
-			<br/>
-		<h2>Things Alfred likes</h2>
-			<ul>
-				<li>Doggos</li>
-				<li>Light-hearted fun</li>
-				<li>Conntecting with like minded people</li>
-				<li>Expressing his opinion</li>
-			</ul>
-		<a href="index.php">Home</a>
+		<p>Alfred is a Dentist. He regularly works long shifts and sometimes just needs a break when he's done for the day.
+			He finds it relaxing to browse r/tippytaps to see some h*ckin good doggos. But he wants to not only see the 			posts, but to interact with them and leave comments to express the overwhelming joy he feels.</p>
+			<a href="index.php">Home</a>
 	</body>
 </html>
