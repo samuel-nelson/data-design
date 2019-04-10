064468909c4fcf4d8d9e95fce475b1b45d077ed0
Samuel Nelson <snelson54@cnm.edu>
2019-04-10 11:54:33 -0600

    Added server response

--- epic/use-case.php
+++ epic/use-case.php
@@ -16,28 +16,28 @@
 			<h3>Steps Alfred must take to be able to make a comment on r/tippytaps</h3>
 				<ul>
 					<li>Navigate to reddit.com/r/tippytaps</li>
-					<li>Server returns subreddit /r/tippytaps to browser</li>
+					<li>*Server returns subreddit /r/tippytaps to browser</li>
 					<li>Click on the post that attracts his attention</li>
-					<li>Server returns post in same tab</li>
+					<li>*Server returns post in same tab</li>
 					<li>Scroll down until the "add comment" section is visible</li>
-					<li>Webpage moves down</li>
+					<li>*Webpage moves down</li>
 					<li>Click on the "add comment" section</li>
-					<li>"Comment" content field goes blank and allows input</li>
+					<li>*"Comment" content field goes blank and allows input</li>
 					<li>Type in whatever is on his mind</li>
-					<li>Server accepts user input and displays what it written</li>
+					<li>*Server accepts user input and displays what it written</li>
 					<li>Click submit</li>
-					<li>Server saves post and displays it on the post user was on</li>
+					<li>*Server saves post and displays it on the post user was on</li>
 				</ul>
 			<h3>Steps Alfred must take to be able to edit a comment</h3>
 				<ul>
 					<li>Scroll down to comment</li>
-					<li>Webpage moves down</li>
+					<li>*Webpage moves down</li>
 					<li>Click on the 'edit' link</li>
-					<li>Server displays previously written comment while accepting changes from end user</li>
+					<li>*Server displays previously written comment while accepting changes from end user</li>
 					<li>Retype or Delete as you wish</li>
-					<li>Server presents edits as they are written</li>
+					<li>*Server presents edits as they are written</li>
 					<li>Click submit</li>
-					<li>Server saves edited post and displays it on the post the user was on</li>
+					<li>*Server saves edited post and displays it on the post the user was on</li>
 				</ul>
 		<a href="index.php">Home</a>
 	</body>
