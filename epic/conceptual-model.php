bea2a1baf62ac1205cf9ea00f5b113d2bd1dee6b
Samuel Nelson <snelson54@cnm.edu>
2019-04-13 16:00:25 -0600

    Changed ERD model and added keys

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -6,7 +6,47 @@
 	</head>
 	<body>
 		<h1>Conceptual Model</h1>
-		<img src="erd.jpg" alt="erd image">
+		<img src="erd.jpeg" alt="erd image">
+		<br/>
+		<h2>Entities & Attributes</h2>
+			<h3>user</h3>
+				<ul>
+					<li>userName (Primary Key)</li>
+					<li>userEmail</li>
+				</ul>
+			<h3>post</h3>
+				<ul>
+					<li>postTitle (Primary Key)</li>
+					<li>postMedia</li>
+					<li>postText</li>
+					<li>postLink</li>
+					<li>postDate</li>
+					<li>postUserName</li>
+					<li>postTag</li>
+					<li>postCommentCount</li>
+					<li>postShare</li>
+					<li>postSave</li>
+					<li>postHide</li>
+					<li>postReport</li>
+					<li>postsubredditId</li>
+				</ul>
+			<h3>postComment</h3>
+				<ul>
+					<li>postCommentText</li>
+					<li>postCommentDate</li>
+					<li>postCommentUserName (Foreign Key)</li>
+					<li>postCommentCount</li>
+					<li>postCommentShare</li>
+					<li>postCommentSave</li>
+					<li>postCommentHide</li>
+					<li>postCommentReport</li>
+				</ul>
+		<h2>Relations</h2>
+			<ul>
+				<li>One <b>User</b> can create may <b>Posts - (1 to m)</b></li>
+				<li>One <b>Post</b> can have many  <b>postComments - (1 to m)</b></li>
+				<li>One <b>User</b> can create many <b>postComments- (1 to m)</b></li>
+			</ul>
 		<br/>
 		<a href="index.php">Home</a>
 	</body>
--- epic/use-case.php
+++ epic/use-case.php
@@ -8,6 +8,15 @@
 		<h1>Use Case</h1>
 		<p>As a casual user, I want to be able to comment the things that are on my mind.</p>
 				<br>
+		<p><b>Preconditions:</b></p>
+				<ul>
+					<li>User has an account and is logged in.</li>
+					<li>Karma is calculated per post and per comment and totaled in userKarma entity.</li>
+				</ul>
+		<p><b>Postconditions:</b></p>
+		<ul>
+			<li>User can successfully leave comments on posts.</li>
+		</ul>
 			<h2>Interaction Flow:</h2>
 			<br>
 			<h3>Steps Alfred must take to be able to make a comment on r/tippytaps</h3>
